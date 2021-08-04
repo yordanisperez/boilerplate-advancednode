4ec7fbcdc2151eb71b1afc537735c8c1aeef9fdd
yordanisperez <yordanis@gmail.com>
2021-08-04 12:17:34 -0400

    WOrking with seccion

--- server.js
+++ server.js
@@ -36,8 +36,16 @@ myDB(async client =>
     app.route('/').get((req, res) => {
       res.render('index', {
         title: 'Connected to Database',
-        message: 'Please login'
+        message: 'Please login',
+        showLogin: true
       });
+    });
+    app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+      res.redirect('/profile');
+    });
+  
+    app.route('/profile').get((req, res) => {
+      res.render('profile');
     });
     passport.serializeUser((user, done) => {
       done(null, user._id);
