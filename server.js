8970452eb783f7e91a660b9a00aeeecd74bcfebc
yordanisperez <yordanis@gmail.com>
2021-08-04 11:10:47 -0400

    WOrking with seccion

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
+const ObjectID = require('mongodb').ObjectID;
 
 const app = express();
 
