0704a9dd7dea96546d5df41acbf58e3070a5cdc6
yordanisperez <yordanis@gmail.com>
2021-08-09 16:47:34 -0400

    ROute /register

--- server.js
+++ server.js
@@ -37,7 +37,8 @@ myDB(async client =>
         res.render('index', {
           title: 'Connected to Database',
           message: 'Please login',
-          showLogin: true
+          showLogin: true,
+          showRegistration: true
         });
       });
       app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
@@ -54,6 +55,38 @@ myDB(async client =>
         req.logout();
         res.redirect('/');
       });
+
+      app.route('/register')
+      .post((req, res, next) => {
+        myDataBase.findOne({ username: req.body.username }, function(err, user) {
+          if (err) {
+            next(err);
+          } else if (user) {
+            res.redirect('/');
+          } else {
+            myDataBase.insertOne({
+              username: req.body.username,
+              password: req.body.password
+            },
+              (err, doc) => {
+                if (err) {
+                  res.redirect('/');
+                } else {
+                  // The inserted document is held within
+                  // the ops property of the doc
+                  next(null, doc.ops[0]);
+                }
+              }
+            )
+          }
+        })
+      },
+        passport.authenticate('local', { failureRedirect: '/' }),
+        (req, res, next) => {
+          res.redirect('/profile');
+        }
+      );
+
 
       passport.serializeUser((user, done) => {
         done(null, user._id);
